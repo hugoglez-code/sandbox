2832717b6cfda160f7a7ee9f9959c4fd40eafdd0
Gonzalez Allende <hugoglez@amazon.es>
2023-05-04 21:29:14 +0200

    Me rindo, se queda el logo

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ export function Header() {
     <Flex justifyContent="center">
       <Image
         alt="logo"
-        src="https://imgur.com/azETeZf"
+        src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
         padding={tokens.space.medium}
       />
     </Flex>
