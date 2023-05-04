7731e61f0fd689719824074178b6b3d267d71787
Gonzalez Allende <hugoglez@amazon.es>
2023-05-04 21:56:01 +0200

    Amplify logo vuelve

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ export function Header() {
     <Flex justifyContent="center">
       <Image
         alt="logo"
-        src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
+        src="https://docs.amplify.aws/assets/logo-dark.svg"
         padding={tokens.space.medium}
       />
     </Flex>
