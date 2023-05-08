4f9489b64934bcfaa6b065faec3352eef3f5a4a8
Gonzalez Allende <hugoglez@amazon.es>
2023-05-08 15:14:38 +0200

    nueva manera de llamar al create a ver

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,12 @@ import { API } from "aws-amplify";
 import * as mutations from './graphql/mutations';
 import * as queries from './graphql/queries';
 
-
-const postDetails = {
-  id: 2,
-  name: 'hola',
-  description: 'Example post'
-};
-
-const createPost = await API.graphql({
+const createPost = API.graphql({
   query: mutations.createPost,
   variables: { input: postDetails }
 });
 
-const listPosts = await API.graphql({ query: queries.listPosts });
+const listPosts = API.graphql({ query: queries.listPosts });
 
 export function Home() {
   const { signOut, user } = useAuthenticator();
@@ -24,7 +17,13 @@ export function Home() {
     <main>
       <h1>Hello {user.username}</h1>
       <button onClick={signOut}>Sign out</button>
-      <button onClick={createPost}>Create example post</button>
+      <button onClick={() => createPost({
+        variables: {
+          id: 2,
+          name: "hola",
+          description: 'Example post'
+        }
+      })}>Create example posy</button>
       <button onClick={console.log(listPosts)}>List posts</button>
     </main>
   );
