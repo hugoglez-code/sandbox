a374d6af0ef09387433c40fe3d2bb8ef62eb46b8
Gonzalez Allende <hugoglez@amazon.es>
2023-05-08 16:46:22 +0200

    a ver

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -15,6 +15,18 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "testgroupGroupRole": "string"
+    }
+  },
+  "function": {
+    "sandboxPostConfirmation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
