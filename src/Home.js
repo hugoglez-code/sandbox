c2bd3b24650dff6c5f926df407be3121c86842d4
Gonzalez Allende <hugoglez@amazon.es>
2023-05-11 17:01:20 +0200

    Fix para el list

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,6 @@ import { API } from "aws-amplify";
 import * as mutations from './graphql/mutations';
 import * as queries from './graphql/queries';
 
-const listPosts = API.graphql({ query: queries.listPosts });
-
 export function Home() {
   const { signOut, user } = useAuthenticator();
 
@@ -23,7 +21,7 @@ export function Home() {
         },
         authMode: "AMAZON_COGNITO_USER_POOLS"
       })}>Create example post</button>
-      <button onClick={console.log(listPosts)}>List posts</button>
+      <button onClick={() => {console.log(API.graphql({ query: queries.listPosts}))}}>List posts</button>
     </main>
   );
 }
